432f2b4029ba56b787fccb0f42903eeeda107556
jiyeon11 <leejiyeon415@gmail.com>
2023-12-24 22:28:00 +0900

    💄 스포츠 종목 선택 화면

---
+++ app/src/main/java/com/cookandroid/noti/EventChoiceActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.noti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_choice);
+    }
+}
--- app/src/main/java/com/cookandroid/noti/Login.java
+++ app/src/main/java/com/cookandroid/noti/Login.java
@@ -5,22 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class Login extends AppCompatActivity {
     ImageButton loginButton;
+    EditText id,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         loginButton = findViewById(R.id.roginButton);
-        Intent intent = new Intent(this, MainActivity.class);
+        id = findViewById(R.id.id);
+        password = findViewById(R.id.password);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        Intent intent = new Intent(this, EventChoiceActivity.class);
+
+        loginButton.setOnClickListener(new View.OnClickListener() { //로그인 버튼 클릭시
             @Override
             public void onClick(View view) {
                 startActivity(intent);
+
             }
         });
     }
